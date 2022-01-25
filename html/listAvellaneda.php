96264b99453fd024039b2f298d7d80729fbc8622
MendioX <82616448+MendioX@users.noreply.github.com>
2022-01-25 15:00:07 -0300

    new menu

--- html/listAvellaneda.php
+++ html/listAvellaneda.php
@@ -5,12 +5,7 @@
 </head>
 <body>
     <?php include("../php/list_user.php") ?>
- <ul>
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+ 
 
 
 <h1>Usuarios Avellaneda</h1>
